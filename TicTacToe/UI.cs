d20a53c4b265756f8bb497e3f1ff7747fc478b02
TheSolian <89124854+TheSolian@users.noreply.github.com>
2022-10-22 16:43:29 +0200

    Set up base structure

---
+++ TicTacToe/UI.cs
@@ -0,0 +1,19 @@
+﻿namespace TicTacToe;
+
+public class UI
+{
+    public Player[] GetPlayerInput()
+    {
+        return null;
+    }
+
+    public void PrintStatus(string[] board)
+    {
+        
+    }
+
+    public void PrintWinner(Player[] playerList)
+    {
+        
+    }
+}
